2bfadca9a7035086920c11da68a243ae6cbfd75c
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2025-09-03 12:35:01 +0530

    ecommerce

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,16 @@
+import HomePage from "@/components/Homepage/homepage";
+import Footer from "@/components/Layout/footer";
+import Header from "@/components/Layout/header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+      <HomePage />
+
+    </>
+  );
+}
+
+
